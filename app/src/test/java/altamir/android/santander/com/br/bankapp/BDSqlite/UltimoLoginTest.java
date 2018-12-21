231e07f030cd47f8d389ef743a54ee9e23859191
Altamir Carvalho <neto.sk8@gmail.com>
2018-12-21 15:58:04 -0300

    The last commit - importando o Robotium - Sem implementos de Testes

---
+++ app/src/test/java/altamir/android/santander/com/br/bankapp/BDSqlite/UltimoLoginTest.java
@@ -0,0 +1,21 @@
+package altamir.android.santander.com.br.bankapp.BDSqlite;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import altamir.android.santander.com.br.bankapp.MainActivity;
+
+
+public class UltimoLoginTest extends MainActivity {
+
+    @Test
+    public void salvar() {
+    }
+
+    @Test
+    public void carregaUltimoLogin() {
+    }
+
+    @Test
+    public void verificaRegistro() {
+    }
+}
